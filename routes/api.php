e02d37c39e91d0bad121887c48a4eded5cc586ff
DwiFatimaA <3103120072@student.smktelkom-pwt.sch.id>
2022-10-24 08:01:44 +0700

    crud siswa

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HeloController;
+use App\Http\Controllers\SiswaController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +16,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//   return $request->user();
+//});
+
+//
+// Task
+// URL : http://localhost:8000/api/halo
+// METHOD: GET
+// Exec: function
+// Return: JSON => {"me": "cantik"}
+//
+Route::get('halo', function(){
+    $data = ["me" => "cantik"];
+
+    return $data;
 });
+
+//Route::get('helocontroller', [HeloController::class, 'index']);
+//Route::post('helocontroller', [HeloController::class, 'store']);
+//Route::delete('helocontroller', [HeloController::class, 'destroy']);
+Route::resource('helocontroller', HeloController::class);
+Route::resource('siswa', SiswaController::class);
+
