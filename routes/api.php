085027085137cb2d89b7bc097df71d10930081ce
DwiFatimaA <3103120072@student.smktelkom-pwt.sch.id>
2022-11-04 10:02:38 +0700

    auth

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\HeloController;
+use App\Http\Controllers\testController;
 use App\Http\Controllers\SiswaController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -21,23 +22,27 @@ use App\Http\Controllers\BookController;
 //   return $request->user();
 //});
 
-//
-// Task
-// URL : http://localhost:8000/api/halo
-// METHOD: GET
-// Exec: function
-// Return: JSON => {"me": "cantik"}
-//
 Route::get('halo', function(){
-    $data = ["me" => "cantik"];
+    $data = ["me" => "Ganteng"];
 
     return $data;
 });
 
-//Route::get('helocontroller', [HeloController::class, 'index']);
-//Route::post('helocontroller', [HeloController::class, 'store']);
-//Route::delete('helocontroller', [HeloController::class, 'destroy']);
-Route::resource('helocontroller', HeloController::class);
-Route::resource('siswa', SiswaController::class);
-Route::resource('books', BookController::class);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request){
+    return $request->user();
+});
 
+//public route
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::resource('testController', testController::class);
+Route::resource('siswa', SiswaController::class);  
+Route::resource('books', BookController::class);  
+
+//protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('books', BookController::class)->except('create', 'edit');
+    Route::post('/logout', [AuthController::class, 'logout']) ;
+    Route::resource('authors', AuthorController::class)->except('create', 'edit');
+});
